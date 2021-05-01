ef856c76a804f4c84268a49057c2fc21bd2f103d
Richard Lopes <richard.lopes1995@gmail.com>
2021-05-01 14:12:03 -0300

    debugging

--- test.py
+++ test.py
@@ -2,13 +2,6 @@ from minips import Minips
 import cProfile, pstats
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-
-    minips = Minips(mem_mode=2)
-    minips.load('./examples/09.contador')
-    minips.execute()
-    print("a")
-=======
     profiler = cProfile.Profile()
     minips = Minips(mem_mode=1)
     minips.load('./examples/example')
@@ -17,4 +10,3 @@ if __name__ == "__main__":
     profiler.disable()
     stats = pstats.Stats(profiler).sort_stats('tottime')
     stats.print_stats()
->>>>>>> feature/profile
